6ff5502ab19a3ff6881296af8675b43521ba4d27
ormaks <maks.chytailo@gmail.com>
2020-08-30 20:48:18 +0300

    Build React boilerplate. React + Webpack + TypeScript + CSS-in-JS.

---
+++ src/components/App.tsx
@@ -0,0 +1,14 @@
+import React, {FC} from 'react';
+import {CustomHeader} from '../styles/CustomHeader';
+
+
+
+const App: FC = () => {
+    return (
+        <div>
+            <CustomHeader>Hello Wworld!</CustomHeader>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/styles/CustomHeader.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components'
+
+export const CustomHeader = styled.h1`
+    color: #292727;
+    text-align: center;
+    `
